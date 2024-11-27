00e1664996ed357b34d8079fc1c884e6d7cbe351
VO Cindy -DARLINGTON <cindy.vo@opg.com>
2024-11-27 07:22:19 -0500

    set up the portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { AboutComponent } from "./about/about.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { ProjectComponent } from "./project/project.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, AboutComponent, NavBarComponent, ProjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent {
+  constructor ( private ViewportScroller: ViewportScroller) {}
+
+  navLink: string[] = ['About', 'Experience', 'Projects', 'Contact']
+
+  scrollTo(section: string) {
+    let elementId = section.toLowerCase().replace(' ', '-');
+    this.ViewportScroller.scrollToAnchor(elementId);
+  }
+}
