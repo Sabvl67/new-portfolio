ee48759092caf9ccbbd01dee7d30da49d7bd8c1a
Sabvl67 <volequynhanh67@gmail.com>
2025-01-07 22:37:31 -0500

    there are errors, timeline works, working on project atm

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,11 +1,22 @@
 
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
+
+interface EventItem {
+  status?: string;
+  date?: string;
+  icon?: string;
+  color?: string;
+  image?: string;
+}
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TimelineModule, CardModule, ButtonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
@@ -15,14 +26,15 @@ experiences: any;
   detailOnClick(experience:any) {
     experience.detailIsDisplayed=!experience.detailIsDisplayed;
   }
-  // events: EventItem[];
+  events: EventItem[];
 
-  // constructor() {
-  //     this.events = [
-  //         { status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0' },
-  //         { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
-  //         { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' },
-  //         { status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B' }
-  //     ];
-  // }
+  constructor() {
+      this.events = [
+          { status: 'Machine Learning Fellow', date: 'May 2023 - Jun 2023', icon: 'pi pi-shopping-cart', color: '#9C27B0' },
+          { status: 'Undergraduate Research Assistant', date: 'Oct 2023 - Aug 2024', icon: 'pi pi-cog', color: '#673AB7' },
+          { status: 'Fintech Cadence Trainee', date: 'Mar 2024 - May 2024', icon: 'pi pi-shopping-cart', color: '#FF9800' },
+          { status: 'Mitacs Globallink Internship Mentor', date: 'Apr 2024 - Aug 2024', icon: 'pi pi-shopping-cart', color: '#4caf50' },
+          { status: 'Software Developer Intern', date: 'Sep 2024 - Apr 2025', icon: 'pi pi-check', color: '#607D8B' }
+      ];
+  }
 }
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -4,6 +4,7 @@ import { RadioButtonModule } from 'primeng/radiobutton';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
+import { WORK_DETAILS } from '../work-detail';
 
 @Component({
   selector: 'app-project',
@@ -12,6 +13,47 @@ import { TagModule } from 'primeng/tag';
   templateUrl: './project.component.html',
   styleUrl: './project.component.css',
 })
-export class ProjectComponent {
 
+
+export class ProjectComponent implements OnInit {
+  products: Product[] | undefined;
+
+  responsiveOptions: any[] | undefined;
+
+  constructor(private workDetails: WORK_DETAILS) {}
+
+  ngOnInit() {
+      this.productService.getProductsSmall().then((products: Product[] | undefined) => {
+          this.products = products;
+      });
+
+      this.responsiveOptions = [
+          {
+              breakpoint: '1199px',
+              numVisible: 1,
+              numScroll: 1
+          },
+          {
+              breakpoint: '991px',
+              numVisible: 2,
+              numScroll: 1
+          },
+          {
+              breakpoint: '767px',
+              numVisible: 1,
+              numScroll: 1
+          }
+      ];
+  }
+
+  getSeverity(status: string) {
+      switch (status) {
+          case 'INSTOCK':
+              return 'success';
+          case 'LOWSTOCK':
+              return 'warning';
+          case 'OUTOFSTOCK':
+              return 'danger';
+      }
+  }
 }
